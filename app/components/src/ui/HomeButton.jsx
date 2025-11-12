3625bd879fd9c27e9ec5466c6054c5b2ea5bf3a4
henrik björserud <henrikbjorserud@gmail.com>
2025-11-12 18:18:20 +0100

    Home menu redesign. Pictograms now in training and also in correct order. Home button implemented. Scoreboard wider. Crossfade transitions.

--- app/components/src/ui/HomeButton.jsx
+++ app/components/src/ui/HomeButton.jsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 
 export default function HomeButton() {
     return (
-        <Link 
-            to="/" 
+        <Link
+            to="/"
             className="inline-flex items-center justify-center w-10 h-10 rounded-full hover:bg-black/5 dark:hover:bg-white/5 transition"
             aria-label="Till startsidan"
         >
